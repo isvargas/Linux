a3e1ed42c8e87b40816042538cfe4fe40d46a109
Ivan S. Vargas <contato@is5.com.br>
2019-04-24 15:40:20 -0300

    Update backup_dbs.sh

--- backup_dbs.sh
+++ backup_dbs.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-#Script para realizar o backup de varios banco de dados PostgreSQL e gravar em um arqivo ZIP.
+#Script para realizar o backup de varios banco de dados PostgreSQL e gravar em um arquivo ZIP.
 #O backup sera gerado no formato de scripts (create, insert).
 #Observacoes
 #1.O pg_dump, por padrao, vai pedir a senha do usuario para realizar a operacao, o que poderia
 #  quebrar a funcao desse script. Para resolver isso podemos configurar um arquivo na home do usuario
 #  com as devidas configuracoes para cada banco. O pg_dump ira buscar neste arquivo a senha do usuario,
 #  por exemplo. 
-#  O arquivo segue o seguinte padrão:
-#        host:porta:banco:user:pass
+#  O arquivo segue o seguinte padrao:
+#     host:porta:banco:user:pass
 #  Podendo ter varias linhas no mesmo.
 #  o arquivo deve se chamar .pgpass, e estar no diretorio home do usuario.
 #  Detalhe: este arquivo deve ser configurado com chmod 0600, impedindo assim que outros
-#  usuario alem de vc tenha permissao de ler o conteudo e descobrir suas senhas :).
+#  usuarios, alem de vc, tenham permissao de ler o conteudo e descobrir suas senhas :).
 #
 #2.Neste exemplo tenho dois arrays: um especificando os servidores, e o outro especificando
 #o nome do banco de dados. Como nas revendas por ai o nome do usuario eh o mesmo nome do banco
@@ -29,7 +29,7 @@ DATABASES=("server1" "server2" "server3")
 
 #realiza o backup
 for ((i=0; i< ${#SERVIDORES[*]}; i++)); do
-  echo "Backupeando ${DATABASES[i]}"
+  echo "[+]Backupeando ${DATABASES[i]}"
   dest="${DATABASES[$i]}-$data.sql"
   pg_dump -h ${SERVIDORES[$i]} -p 5432 -U ${DATABASES[$i]} -F plain -f $dest ${DATABASES[$i]}
 done
